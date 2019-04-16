d52309c032223f226ab85b62ba80673d45326e60
ajpendergrass <ajpendergrass@gmail.com>
2019-04-16 16:21:16 -0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,9 +4,8 @@
 
 # Don't forget to reach out for help after your own due diligence
 
-year = int()
-
 def is_leap(year):
+    year = int()
     if year % 100 == 0 and year % 400 == 0: 
        return True
     elif year % 100 != 0 and year % 4 == 0:
